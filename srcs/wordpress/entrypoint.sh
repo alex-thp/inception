a391b5944cefd64906d08a801b785aa6ea17ce31
alex_maison <alex2temmerman@gmail.com>
2022-07-20 12:55:48 +0200

    wordpress is ready; admin part to configure

--- srcs/wordpress/entrypoint.sh
+++ srcs/wordpress/entrypoint.sh
@@ -4,12 +4,16 @@ Entering script
 
 "
 set -xv
-apt-get install sudo
-sudo apt-get install less
-export PATH=$PATH:/usr/local/mysql/bin
-cd /var/www/ 
+chown -R www-data /var/www/html
+#apt-get install sudo
+#sudo apt-get install less
+#export PATH=$PATH:/usr/local/mysql/bin
+cd /var/www/html
+#wp core download --allow-root
+#wp core install --url=ade-temm.42.fr --title="WP-CLI" --admin_user=wordpress_user --admin_password=wordpress_secret --admin_email=alexandre2t@hotmail.fr --allow-root
 wp core download --allow-root
-wp core install --url=ade-temm.42.fr --title="WP-CLI" --admin_user=wordpress_user --admin_password=wordpress_secret --admin_email=alexandre2t@hotmail.fr --allow-root
+wp config create --dbname=dbwordpress --dbuser=wordpress_user --dbpass=wordpress_secret --dbhost=container_mariadb --allow-root
+wp core install --url=ade-temm.42.fr --title=Mon_site --admin_user=super_user --admin_password=super_secret --admin_email=admin@admin.com --allow-root
 wp user create --allow-root super_user alexandre2t@hotmail.fr --user_pass=super_secret
 
 
