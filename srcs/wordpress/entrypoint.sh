a60320d1bf94028f64d74bff8e2ca5054f8e428e
alex_maison <alex2temmerman@gmail.com>
2022-07-23 13:55:42 +0200

    env on wordpress

--- srcs/wordpress/entrypoint.sh
+++ srcs/wordpress/entrypoint.sh
@@ -12,9 +12,10 @@ cd /var/www/html
 #wp core download --allow-root
 #wp core install --url=ade-temm.42.fr --title="WP-CLI" --admin_user=wordpress_user --admin_password=wordpress_secret --admin_email=alexandre2t@hotmail.fr --allow-root
 wp core download --allow-root
-wp config create --dbname=dbwordpress --dbuser=wordpress_user --dbpass=wordpress_secret --dbhost=container_mariadb --allow-root
-wp core install --url=ade-temm.42.fr --title=Mon_site --admin_user=super_user --admin_password=super_secret --admin_email=admin@admin.com --allow-root
-wp user create wordpress_user alexandre2t@hotmail.fr --role=author --user_pass=wordpress_secret --allow-root
+wp core install --url=$URL --title=$WORDPRESS_SITENAME --admin_user=$WORDPRESS_SUPER --admin_password=$WORDPRESS_SUPER_SECRET --admin_email=$SUPER_EMAIL --allow-root
+#wp core install --url=ade-temm.42.fr --title=Mon_site --admin_user=super_user --admin_password=super_secret --admin_email=admin@admin.com --allow-root
+wp user create nouveau alexandre2t@hotmail.fr --role=author --user_pass=$NEW_PASS --allow-root
+#wp user create wordpress_user alexandre2t@hotmail.fr --role=author --user_pass=wordpress_secret --allow-root
 
 
 /usr/sbin/php-fpm7.3 --nodaemonize
